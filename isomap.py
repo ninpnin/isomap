d40cb0116f2f3efeec21a629c5100fbaa9dc6c47
ninpnin <vaino.yrjanainen@aalto.fi>
2018-04-23 14:02:34 +0300

    Refactor into multiple functions

--- isomap.py
+++ isomap.py
@@ -18,6 +18,7 @@ def main():
     parser.add_argument('--out_dim', type=str, default=2, help='dimensionality of the output')
     args = parser.parse_args()
 
+    #read data from csv, convert to data matrix
     colnames = ['s', 'x1', 'x2']
     data = pandas.read_csv('toydata.csv', names=colnames)
     x0 = data.x1.tolist()[1:1000]
@@ -31,13 +32,10 @@ def main():
     data = np.column_stack((x,y))
     df = pandas.DataFrame(data, columns=['xcord', 'ycord'])
 
+    #calculate pairwise distances
     dist_matrix = pandas.DataFrame(distance_matrix(df.values, df.values), index=df.index, columns=df.index).values
 
-    print(df)
-    print(dist_matrix)
-
-    print(dist_matrix)
-
+    #find k closest neighbors for each data point
     D0 = np.zeros(dist_matrix.shape)
 
     for index in range(0, dist_matrix.shape[0]):
@@ -47,74 +45,61 @@ def main():
     	for k_index in k_smallest:
     		D0[k_index, index] = dist_matrix[k_index, index]
 
-    print("D0:")
-    print(D0)
-    #a = np.column_stack((x, y))
-    a = np.array([[0,1,1,1],[1,0,1,1],[1,1,0,1],[1,1,1,0]])
-    print(a)
-    b = sparse.csgraph.shortest_path(D0,'D',False)
-    print(b)
+    #find distances in graph of k neighbors (dijkstra)
+    shortest_paths = sparse.csgraph.shortest_path(D0,'D',False)
+    print(shortest_paths)
 
-    dp = gradient_descent(b, args.out_dim, z)
-    print(dp)
+    #minimize error in distances in a lower dimensional space
+    X = gradient_descent(shortest_paths, args.out_dim)
 
-def gradient_descent(D0, dim, color):
-	datapoints = D0.shape[0]
-	print("datapoints " + str(datapoints))
+    plot_data(X, z)
 
-	print("out dim " + str(dim))
+def gradient_descent(D0, dim):
+	data_length = D0.shape[0]
 
-	D = np.random.rand(datapoints, datapoints)
-	X = np.random.rand(datapoints, dim)
+	#distance matrix in lower dimensional space
+	D = np.random.rand(data_length, data_length)
+	#data matrix in lower dimensional space
+	X = np.random.rand(data_length, dim)
 
-	print("X: ")
-	print(X)
-	print("D: ")
-	for iteration in range(0, 250):
-		print("Iteration: ")
-		print(iteration)
+	for iteration in range(0, 100):
+		print("Iteration: " + str(iteration))
+
 		#update distance matrix
-
 		df = pandas.DataFrame(X, columns=['xcord', 'ycord'])
 		D = pandas.DataFrame(distance_matrix(df.values, df.values), index=df.index, columns=df.index).values
 
-		print("D: ")
-		print(D)
-
+		#calculate gradient
 		d_d = (D0 - D) / D
 		np.fill_diagonal(d_d,0)
-
-		d_d_rowsum = d_d @ np.ones((datapoints,dim))
+		d_d_rowsum = d_d @ np.ones((data_length,dim))
 		gradient = (d_d @ X - d_d_rowsum * X)* 2
+
+		#normalize gradient to unit length
 		magnitude = math.sqrt(np.sum(gradient**2))
 		gradient = gradient / magnitude
-		print(d_d)
-		print("gradient: ")
-		print(gradient)
-		#gradient2 = 
-		X = X - (gradient * 0.8)
+
+		#update X
+		X = X - (gradient * 0.2)
 
 	print("Updated X: ")
 	print(X)
 
-	print("D")
+	print("Pairwise distances in reduced dimension")
 	print(D)
 
-	vector1 = X[:,0]
-	vector2 = X[:,1]
-	vector3 = vector2
+	return X
 
-	cm = plt.cm.get_cmap('RdYlBu')
+def plot_data(data_matrix, color):
+    vector1 = data_matrix[:,0]
+    vector2 = data_matrix[:,1]
 
-	min_x = np.amin(vector1)-2
-	max_x = np.amax(vector1)+2
+    cm = plt.cm.get_cmap('RdYlBu')
 
-	vector1
-	sc = plt.scatter(vector1, vector2, c=color, vmin=0, vmax=1, s=2, cmap=cm)
-	plt.colorbar(sc)
-	plt.show()
+    sc = plt.scatter(vector1, vector2, c=color, vmin=0, vmax=1, s=2, cmap=cm)
+    plt.colorbar(sc)
+    plt.show()
 
-	return datapoints
 
 if __name__ == '__main__':
 	main()
