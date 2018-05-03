7a4cf1e0963f4c63fe471b91235c9ae988b758c5
ninpnin <vaino.yrjanainen@aalto.fi>
2018-05-03 17:10:31 +0300

    Generalize to n dimensional input data

--- isomap.py
+++ isomap.py
@@ -16,15 +16,26 @@ def main():
     parser.add_argument('--filename', type=str, default="toydata.csv", help='path of data file')
     parser.add_argument('--k', type=int, default=11, help='number of nearest neighbors for the k-means')
     parser.add_argument('--out_dim', type=int, default=2, help='dimensionality of the output')
+    parser.add_argument('--color_column', type=int, default=0, help='which column in the data represents the color')
+    parser.add_argument('--max_iterations', type=int, default=300, help='how many iterations will be performed for the gradient descent')
+    parser.add_argument('--neighborhood_radius', type=float, default=0.15, help='how many iterations will be performed for the gradient descent')
     args = parser.parse_args()
 
     #read data from csv, convert to data matrix
-    data = pandas.read_csv('toydata.csv').values
-
-    #delete label row
-    data = np.delete(data, 0, 0).astype(np.float)
+    data = pandas.read_csv(args.filename).values
     print(data)
-    z = data[:,0]
+    #delete label row
+    z = data[:,args.color_column].astype(np.float)
+    for i in range(0,args.color_column+1):
+        data = np.delete(data, 0, 1)
+        print("#fdjnksnkjfsd")
+        print(data)
+    print(data)
+    data = data.astype(np.float)
+
+    print(data)
+    print("Z")
+    print(z)
     df = pandas.DataFrame(data)
 
     #calculate pairwise distances
@@ -35,27 +46,32 @@ def main():
 
     for index in range(0, dist_matrix.shape[0]):
     	vector = dist_matrix[:,index]
-    	k_smallest = np.argpartition(vector, args.k)[:args.k]
+    	k_smallest = np.argpartition(vector, args.k)[:args.k+1]
     	for k_index in k_smallest:
-    		D0[k_index, index] = dist_matrix[k_index, index]
+            D0[k_index, index] = dist_matrix[k_index, index]
+            D0[index, k_index] = dist_matrix[k_index, index]
 
     #find distances in graph of k neighbors (dijkstra)
+    print("D0")
+    print(D0)
     shortest_paths = sparse.csgraph.shortest_path(D0,'D',False)
 
     #minimize error in distances in a lower dimensional space
-    X = gradient_descent(shortest_paths, args.out_dim)
+    X = gradient_descent(shortest_paths, args.out_dim, args.max_iterations)
 
-    rc = recall(dist_matrix,d_matrix(X),0.1)
-    pr = precision(dist_matrix,d_matrix(X),0.1)
+    rc = recall(dist_matrix,d_matrix(X),args.neighborhood_radius)
+    pr = precision(dist_matrix,d_matrix(X),args.neighborhood_radius)
     
     print("precision:")
     print(pr)
     print("recall:")
     print(rc)
 
+    np.savetxt('isomap.csv', X, delimiter=';', fmt='%1.3f')
+
     plot_data(X, z)
 
-def gradient_descent(D0, dim):
+def gradient_descent(D0, dim, iterations):
     data_length = D0.shape[0]
 
     #distance matrix in lower dimensional space
@@ -63,9 +79,7 @@ def gradient_descent(D0, dim):
     #data matrix in lower dimensional space
     X = np.random.rand(data_length, dim)
 
-    iterations = 500
     for iteration in range(0, iterations):
-        print("Iteration: " + str(iteration) + " / " + str(iterations))
 
         #update distance matrix
         df = pandas.DataFrame(X)
@@ -79,10 +93,15 @@ def gradient_descent(D0, dim):
 
         #normalize gradient to unit length
         magnitude = math.sqrt(np.sum(gradient**2))
-        gradient = gradient / magnitude
+        gradient = gradient #/ magnitude
 
         #update X
-        X = X - (gradient * 0.5)
+        X = X - (gradient * 0.001)
+
+        if iteration % 10 == 0:
+            print("Iteration: " + str(iteration) + " / " + str(iterations))
+            print("Precision: " +str(precision(D0,D,0.15)))
+            print("Recall: " + str(recall(D0,D,0.15)))
 
     print("Updated X: ")
     print(X)
@@ -98,7 +117,7 @@ def plot_data(data_matrix, color):
 
     cm = plt.cm.get_cmap('RdYlBu')
 
-    sc = plt.scatter(vector1, vector2, c=color, vmin=0, vmax=1, s=2, cmap=cm)
+    sc = plt.scatter(vector1, vector2, c=color, s=7, cmap=cm)
     plt.colorbar(sc)
     plt.show()
 
@@ -113,12 +132,15 @@ def intersect(b1, b2):
 
 def precision(o, re, radius):
 
+    print(o)
     original = np.copy(o)
     reduction = np.copy(re)
 
     #1 true, 0 false
+    original[original == 0] = -1
     original[original > radius] = 0
     original[original > 0] = 1
+    original[original == -1] = 1
 
     #2 true, -2 false
     reduction[reduction > radius] = -2
@@ -138,7 +160,7 @@ def precision(o, re, radius):
     for i in range(0,n):
         c = np.argwhere(combined[i,:] == 1).tolist()
         o = np.argwhere(original[i,:] == 1).tolist()
-        inter = len(intersect(c,o))
+        inter = len(c)
         pr += inter / len(o)
 
     return pr / n
@@ -149,8 +171,11 @@ def recall(o, re, radius):
     reduction = np.copy(re)
 
     #1 true, 0 false
+    original[original == 0] = -1
     original[original > radius] = 0
     original[original > 0] = 1
+    original[original == -1] = 1
+
 
     #2 true, -2 false
     reduction[reduction > radius] = -2
